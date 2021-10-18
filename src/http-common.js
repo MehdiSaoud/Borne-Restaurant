606251b54f9b103ae83090487822bb046d18940a
Mehdi Saoud <saoudmehdi5@gmail.com>
2021-10-18 15:28:08 +0200

    correction

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://borne-reactjs.herokuapp.com',
+    baseURL: 'https://borne-reactjs.herokuapp.com',
     headers : {
         'Content-type' : 'application/json'
     }
